e729cb5d776ead8fb16ec7e2817b973787285d04
a7xnico <pablonicolasarayamaza@gmail.com>
2025-11-14 02:44:33 -0300

    cambios a Admin

--- src/Agencia/Gestores/Autenticador.java
+++ src/Agencia/Gestores/Autenticador.java
@@ -1,12 +1,27 @@
 package Agencia.Gestores;
 
+import Agencia.GestionArchivos.GestorJSONAdmin;
 import Agencia.Modelo.Usuarios.Admin;
 
 public class Autenticador {
     private Admin admin;
+    private GestorJSONAdmin gestorJson;
 
     public Autenticador(){
-        this.admin = new Admin("admin", "admin123");
+        gestorJson = new GestorJSONAdmin();
+        cargarAdmin();
+    }
+
+    private void cargarAdmin(){
+        admin = gestorJson.deserializarAdmin();
+        if (admin == null) {
+            admin = new Admin("admin", "admin123");
+            guardarAdmin();
+        }
+    }
+
+    private void guardarAdmin(){
+        gestorJson.serializarAdmin(admin);
     }
 
     public boolean validarAdmin(String username, String password){
@@ -14,7 +29,9 @@ public class Autenticador {
     }
 
     public void cambiarCredenciales(String nuevoUsername, String nuevoPassword){
-        this.admin = new Admin(nuevoUsername, nuevoPassword);
+        admin = new Admin(nuevoUsername, nuevoPassword);
+        guardarAdmin();
+        System.out.println("Credenciales del admin actualizadas exitosamente");
     }
 
     public Admin getAdmin(){
